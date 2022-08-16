aada8a8ccb031a535a16f28425176a86abf8bbac
Shntel Thomas <shantelthomas2023@gmail.com>
2022-08-16 00:31:09 -0400

    adjusting main

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from checkers.game import Game
 from checkers.constants import *
 from checkers.board import Board
-from startpage.startwindow import Piece_menu
+from startpage.startwindow import Piece_menu, main_menu
 from startpage.button  import Button
 
 FPS = 60
@@ -18,41 +18,7 @@ def get_row_col_from_mouse(pos):
 
 def main():
 
-    #game variables
-    game_paused = False
-
-    run_startpage = True
-    while run_startpage:
-
-        SCREEN.fill(WHITE)
-        SCREEN.blit(STARTPAGE_BK, (STARTPAGE_BK_rect))
-
-        
-        #print button
-        if game_paused == False:
-             if SELECPLAYER_BTN.draw(SCREEN):
-                Piece_menu()
-             if LEVELS_BTN.draw(SCREEN):
-                game_paused = True
-             if PLAY_BTN.draw(SCREEN):
-                run_startpage = False
-             if QUITE_BTN.draw(SCREEN):
-                pygame.quit()  
-        else:
-            SELECPLAYER_BTN.draw(SCREEN)
-            
-
-        #event handler
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    game_paused = False
-            #if event.type == pygame.QUIT:
-                # run = False
-
-        pygame.display.update()
-
-    
+    main_menu()
 
     run = True
     clock = pygame.time.Clock() 
